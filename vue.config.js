a19ce73ee92f6f175549668e9519b47ecbbccadb
gcali <gcali.git@gmail.com>
2019-08-16 12:29:44 +0200

    Fix base url and add deploy command

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
-module.exports = {
-    configureWebpack: {
-        devtool: 'source-map'
+if (process.env.NODE_ENV === "production") {
+    module.exports = {
+        baseUrl: '/bowling-solitaire',
+        configureWebpack: {
+            devtool: 'source-map'
+        }
+    }
+}
+else {
+    module.exports = {
+        configureWebpack: {
+            devtool: 'source-map'
+        }
     }
 }
