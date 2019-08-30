aef3a53b0940a1771486d7370dce2c1fe93acd70
gcali <gcali.git@gmail.com>
2019-08-30 17:21:47 +0200

    Remove old hello test folder

--- src/server/hello/hello.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HelloService } from './hello.service';
-
-@Module({
-  providers: [HelloService],
-  exports: [HelloService]
-})
-export class HelloModule { }
