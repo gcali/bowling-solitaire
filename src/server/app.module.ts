1144a8cfc28c8064c3f9505971d35ac0498b54d8
gcali <gcali.git@gmail.com>
2019-09-09 18:05:05 +0200

    Add docker

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [GameModule],
+  imports: [
+    GameModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 8998,
+      username: 'postgres',
+      password: 'password',
+      database: 'db',
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
