10492229f3a2a4aebdd07194c5ecc2beb4c7b242
gicali <gcali.git@gmail.com>
2019-09-25 17:57:56 +0200

    1bb0da3

---
+++ precache-manifest.a81afeddc4e9faf6d2115a07deafca42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bowling-solitaire/robots.txt"
+  },
+  {
+    "revision": "452a010840cea12047eaa14423dee043",
+    "url": "/bowling-solitaire/logo-512.png"
+  },
+  {
+    "revision": "7ee5995c6dc72ec56bd19eea6c0e7137",
+    "url": "/bowling-solitaire/logo-192.png"
+  },
+  {
+    "revision": "e640bc185a2f91c3c93a020f48e81275",
+    "url": "/bowling-solitaire/logo-1200.png"
+  },
+  {
+    "revision": "ec5ec8957d01e21fb157",
+    "url": "/bowling-solitaire/js/chunk-vendors.71280362.js"
+  },
+  {
+    "revision": "338a5c72489d63c81ec2",
+    "url": "/bowling-solitaire/js/app.321dbebe.js"
+  },
+  {
+    "revision": "02e59750c9660fe4ebd0",
+    "url": "/bowling-solitaire/js/about.671165b4.js"
+  },
+  {
+    "revision": "afd623f242c8676ed8c4ca6d2aef121e",
+    "url": "/bowling-solitaire/index.html"
+  },
+  {
+    "revision": "ec5ec8957d01e21fb157",
+    "url": "/bowling-solitaire/css/chunk-vendors.98a5c11f.css"
+  },
+  {
+    "revision": "338a5c72489d63c81ec2",
+    "url": "/bowling-solitaire/css/app.98de03e0.css"
+  }
+];
